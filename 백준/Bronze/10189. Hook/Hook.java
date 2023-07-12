1dddf67cfdd182c40b56f59b1824be863eeb26bf
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2023-07-12 20:59:17 +0900

    [Bronze V] Title: Hook, Time: 112 ms, Memory: 13836 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileInputStream;
+class Main {
+	public static void main(String args[]) throws Exception {
+		System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+	}
+}
