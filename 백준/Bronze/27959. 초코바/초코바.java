12fb7445dbb1baf5675dea07c301862a1beb9eb3
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2023-07-25 20:47:48 +0900

    [Bronze V] Title: 초코바, Time: 220 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a * 100 >= b){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
