b75424e0fe4fb0f595c6788a4db19e4832a37e30
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2023-08-04 19:56:01 +0900

    [Silver IV] Title: solved.ac, Time: 376 ms, Memory: 37760 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        int cut = (int)Math.round(15 * N * 0.01);
+        int sum = 0;
+        for(int i=cut; i<N-cut; i++){
+            sum += arr[i];
+        }
+        bw.write(Math.round(sum / (double)(N - cut * 2)) + "");
+        bw.flush();
+        bw.close();
+    }
+}
