e92ca99d84dffb42ca38939f56b9172eabb7a968
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2023-08-12 23:22:33 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 220 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,22 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N  = sc.nextInt();
+        for(int i=N; i>0; i--){
+            System.out.println("*".repeat(i));
+        }
+    }
+}
