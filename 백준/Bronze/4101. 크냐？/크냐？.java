a64a09a928777057d2a742da12feacb9b1dae2c3
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2023-08-13 23:58:56 +0900

    [Bronze V] Title: 크냐?, Time: 236 ms, Memory: 17876 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a == 0 & b == 0){
+                break;
+            }
+            else if(a > b){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        }
+    }
+}
