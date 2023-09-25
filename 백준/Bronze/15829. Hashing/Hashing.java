0944053a59593f869e65fc1c5a2db89edd49e2dc
nahyeonung <70554776+nahyeonung@users.noreply.github.com>
2023-09-25 21:27:12 +0900

    [Bronze II] Title: Hashing, Time: 212 ms, Memory: 17852 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,6 +3,7 @@ import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -15,12 +16,14 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		sc.nextLine();
-		int sum = 0;
+		BigInteger sum = new BigInteger("0");
 		String str = sc.nextLine();
 		for(int i=0; i<str.length(); i++){
-			sum += (str.charAt(i) - 96) * (int)Math.pow(31, i);
+			sum = sum.add((BigInteger.valueOf(str.charAt(i) - 96)).multiply((BigInteger.valueOf(31).pow(i))));
+
 		}
-		System.out.println(sum);
+		System.out.println(sum.remainder(BigInteger.valueOf(1234567891)));
+		
 	}
 	
 }
